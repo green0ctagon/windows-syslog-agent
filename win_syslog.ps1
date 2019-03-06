a14c882e0db445cc86df35603f39361ea28769fa
green0ctagon <36968430+green0ctagon@users.noreply.github.com>
2019-03-05 22:56:38 -0500

    Update win_syslog.ps1

--- win_syslog.ps1
+++ win_syslog.ps1
@@ -1,97 +1,184 @@
-# Windows Event Forwarder w/ Remote Logging
-# Compatible w/ PowerShell 2.0-4.0 (win7,2008,2012,etc)
-# Compatible w/ Syslog/Rsyslog/any UDP syslog server
-# Written: Lee Mazzoleni, OSCP, GPEN
+# [!] Windows Syslog Agent
+# [$] Forward Windows Events via UDP in JSON format, designed for compatibility with Syslog/Rsyslog
+# [$] Written: Lee Mazz
 
-"Logging service started"
 
-$time = Get-Date
-$hr = $time.TimeOfDay.Hours
-$min = $time.TimeOfDay.Minutes
-$day = $time.DayOfYear
-$yr = $time.Year
+# [*] New Features:
+	# 1.) Event queuing functionality for offline logging has been added
+		# [x] At runtime, if the program cannot validate the connection with the syslog server, it will log events locally (to a file).  The next time the program runs and can positively confirm connectivity with the syslog server all previously-queued logs will get forwarded.
+		# [x] This feature is designed to accomodate mobile workstations such as laptops that may be used offsite
+		# [x] This feature ensures that no events get dropped due to connectivity issues
+	# 2.) Program has been modularized
 
-$ip = "192.168.68.153"  # rsyslog server 
-$port = 514		# server port
-$address = [system.net.IPAddress]::Parse($ip)
-$server = New-Object System.Net.IPEndPoint $address, $port
 
-$new_socket = [System.Net.Sockets.AddressFamily]::InterNetwork 
-$socket_type = [System.Net.Sockets.SocketType]::Dgram 
-$protocol = [System.Net.Sockets.ProtocolType]::UDP 
-$sock = New-Object System.Net.Sockets.Socket $new_socket, $socket_type, $protocol
-$sock.TTL = 90
+function getStatus () {
 
-# Check to see which logging facilities contain entries (inactive services will produce 0 logs)
-# Facilities with log entries will be appended to the $log_pool array for later iterations
-$log_pool = @()
-Get-WinEvent -ListLog * | ForEach-Object { 
-	if ($_.RecordCount -gt 0) {
-		$log_pool += $_.LogName
-	}
-}
+    $ip = '192.168.11.12'
+    $offlineLogs = 'queued.log'
 
-while ("ok") {
-	$current_time = Get-Date
-	$current_hr  = $current_time.TimeOfDay.Hours
-	$current_min = $current_time.TimeOfDay.Minutes
-    $current_day = $current_time.DayOfYear
-    $current_year = $current_time.Year
-	if ($min -eq $current_min) {
-		sleep 60;
-	} else {
-		$time = $current_time;
-		$previous_min = $time.TimeOfDay.Minutes - 1
-		$hour = $time.TimeOfDay.Hours
-        $day = $time.DayOfYear
-        $year = $time.Year
-		if ($previous_min -eq -1) {
-			$previous_min = 59;
-		} ElseIf ($($hour - 1) -eq -1)  {
-			$hour = 23;
-		} 
-		#$target_time = -join $($hour,':',$previous_min)
-		$event_pool = @()
-        $log_pool | ForEach-Object {
-            Get-WinEvent -LogName "$_" -MaxEvents 20 | Sort-Object TimeCreated | Where-Object { $_.TimeCreated.Hour -eq $hour -and $_.TimeCreated.Minute -eq $previous_min -and $_.TimeCreated.DayOfYear -eq $day -and $_.TimeCreated.Year -eq $year  } | ForEach-Object {
-                $event_pool += $_
+    # check the connectivity to the syslog server before blasting udp at it
+    ping -n 1 $ip | Out-Null
+
+    if ($?) {
+    
+        # if the connection status is verified, check if there is a queued log file that needs to be forwarded
+        ls $offlineLogs | Out-Null
+    
+        if ($?) {
+            $port = 2514
+            $address = [system.net.IPAddress]::Parse($ip)
+            $server = New-Object System.Net.IPEndPoint $address, $port
+            $new_socket = [System.Net.Sockets.AddressFamily]::InterNetwork
+            $socket_type = [System.Net.Sockets.SocketType]::Dgram
+            $protocol = [System.Net.Sockets.ProtocolType]::UDP
+            $sock = New-Object System.Net.Sockets.Socket $new_socket, $socket_type, $protocol
+            $sock.TTL = 90
+            $Enc = [System.Text.Encoding]::ASCII
+            # send all the queued logs to the SIEM
+            gc $offlineLogs | ForEach-Object {
+                $Buffer = $Enc.GetBytes($_)
+                $sock.Connect($server)
+                $sendit = $sock.Send($Buffer)
             }
-        }    
-        if ($event_pool.Count -ne 0) {
-	    $sleepyTime = Get-Random(1..10)
-            sleep $sleepytime
-            $arr_count = $event_pool.Count
-            -join $("Found ",$arr_count," log entries from the previous minute.")
-            $event_pool = $event_pool | Sort-Object { $_.TimeCreated }
-            $event_pool | ForEach-Object {           
-                $message = $_.Message -split "`n"
-                $message = $message[0]
-                $event_id = $_.Id
-                #get acount name from authentication logs
-                if ($event_id -eq 4672 -or $event_id -eq 4634) {
-                    $user = $($($_.Message | findstr Name) -split "`t")[3]
-                } ElseIf ($event_id -eq 4624 -or $event_id -eq 4648) {
-                    $user = $($($($($_.Message | findstr Name) -split "`n")[1]) -split "`t")[3]
-                } Else {
-                    $user = $_.UserId
-                    if (!$user) {
-                        $user = "n/a"
-                    }
-                }
-                $source = $_.ProviderName
-                $hname = $_.MachineName
-                $severity = $_.LevelDisplayName
-                $timestamp = $_.TimeCreated
-		$log = -join $('HostName:"',$hname,'" TimeStamp:"',$timestamp,'" LogType:"',$source,'" ','SrcUser:"',$user,'" Message:"',$message,'" EventID:"',$event_id,'" Severity:"',$severity,'"')
-                #$log = -join $($hname,' - ',$timestamp,' - src_app:','"',$source,'" ','src_user:','"',$user,'" ','msg:','"',$message,'" ','eventID:','"',$event_id,'" ','type:','"',$severity,'" ')
-                $Enc = [System.Text.Encoding]::ASCII
-                $Buffer = $Enc.GetBytes($log)
-	            $sock.Connect($server)
-	            $sendit = $sock.Send($Buffer)
-            } 
-        } else { 
-                echo "No logs this time, going to sleep."
+            # remove the queued logs from disk once they have been forwarded to SIEM
+            ri $offlineLogs -ea 0
         }
-        sleep 60;
-	}
+        
+        #after forwarding any queued logs, resume the normal real-time forwarding
+        logRemotely
+    } else {
+        logLocally
+    }
 }
+
+
+function logRemotely () {
+
+    $providers = New-Object System.Collections.ArrayList
+    Get-WinEvent -ListLog * | ForEach-Object {
+            if ($_.RecordCount -gt 0) {
+                    $providers.Add($_.LogName) | Out-Null
+            }
+    }
+
+    $events = New-Object System.Collections.ArrayList
+    foreach ($fac in ($providers)) {
+        Get-WinEvent -FilterHashtable @{Logname="$fac";StartTime=$time} -ea 0 | ForEach-Object { $events.Add($_) | Out-Null }
+    }
+
+    if ($events.Count -gt 0) {
+
+        $ip = "10.2.113.150"
+        $port = 2514
+        $address = [system.net.IPAddress]::Parse($ip)
+        $server = New-Object System.Net.IPEndPoint $address, $port
+        $new_socket = [System.Net.Sockets.AddressFamily]::InterNetwork
+        $socket_type = [System.Net.Sockets.SocketType]::Dgram
+        $protocol = [System.Net.Sockets.ProtocolType]::UDP
+        $sock = New-Object System.Net.Sockets.Socket $new_socket, $socket_type, $protocol
+        $sock.TTL = 90
+    
+        # Un-comment the two lines below if you are implementing this agent on >50 hosts
+        #$sleepyTime = Get-Random(1..10)tail 
+        #sleep $sleepytime
+
+        $propertiez = @("Message","Id","ProviderName","Logname","ProcessId","MachineName","UserId","TimeCreated","LevelDisplayName","TaskDisplayName","KeywordsDisplayNames")
+        $events = $events | Sort-Object { $_.TimeCreated }
+        $events | ForEach-Object {
+            $log = New-Object System.Object 
+            foreach ($prop in ($propertiez)) { 
+	            $value = $_.$prop -replace '`r`n','`n'
+                $value = $value -replace '`n',' '
+                $value = $value -replace '`t',' '
+                $value = $value -replace '\s+',' '
+                $value = $value -replace '[{}]',''
+                $value = $value -replace '"','~'
+                $value = $value -replace "'",'~'
+                $value = $value.replace('\','/')
+                if (!$value) {
+                    $value = 'null'
+                } ElseIf ($prop -ne "TimeCreated") {
+		            $log | Add-Member NoteProperty $prop $value
+	            } else {
+		            [string]$value = $_.TimeCreated
+		            $log | Add-Member NoteProperty $prop $value
+	            }
+            }
+        
+            $jsonLog = ConvertTo-Json -InputObject $log
+            $jsonLog = $jsonLog -replace '`r`n','`n'
+            $jsonLog = $jsonLog -replace '`n',' '
+            $jsonLog = $jsonLog -replace '`t',' '
+            $jsonLog = $jsonLog -replace '\s+',' '
+            $jsonLog = ((($jsonLog.replace("\n"," ")).replace("\t"," ")).replace("\r"," "))
+            #$jsonLog
+
+            # Send the new event log to the syslog/cloud collector server        
+            $Enc = [System.Text.Encoding]::ASCII
+            $Buffer = $Enc.GetBytes($jsonLog)
+            $sock.Connect($server)
+            $sendit = $sock.Send($Buffer)
+        }
+    }
+}
+
+
+function logLocally () {
+    
+    $providers = New-Object System.Collections.ArrayList
+    Get-WinEvent -ListLog * | ForEach-Object {
+            if ($_.RecordCount -gt 0) {
+                    $providers.Add($_.LogName) | Out-Null
+            }
+    }
+
+    $events = New-Object System.Collections.ArrayList
+    foreach ($fac in ($providers)) {
+        Get-WinEvent -FilterHashtable @{Logname="$fac";StartTime=$time} -ea 0 | ForEach-Object { $events.Add($_) | Out-Null }
+    }
+
+    if ($events.Count -gt 0) {
+    
+        # Un-comment the two lines below if you are implementing this agent on >50 hosts
+        #$sleepyTime = Get-Random(1..10)
+        #sleep $sleepytime
+
+        $propertiez = @("Message","Id","ProviderName","Logname","ProcessId","MachineName","UserId","TimeCreated","LevelDisplayName","TaskDisplayName","KeywordsDisplayNames")
+        $events = $events | Sort-Object { $_.TimeCreated }
+        $events | ForEach-Object {
+            $log = New-Object System.Object
+            foreach ($prop in ($propertiez)) { 
+	            $value = $_.$prop -replace '`r`n','`n'
+                $value = $value -replace '`n',' '
+                $value = $value -replace '`t',' '
+                $value = $value -replace '\s+',' '
+                $value = $value -replace '[{}]',''
+                $value = $value -replace '"','~'
+                $value = $value -replace "'",'~'
+                $value = $value.replace('\','/')
+                if (!$value) {
+                    $value = 'null'
+                } ElseIf ($prop -ne "TimeCreated") {
+		            $log | Add-Member NoteProperty $prop $value
+	            } else {
+		            [string]$value = $_.TimeCreated
+		            $log | Add-Member NoteProperty $prop $value
+	            }
+            }
+            $jsonLog = ConvertTo-Json -InputObject $log
+            $jsonLog = $jsonLog -replace '`r`n','`n'
+            $jsonLog = $jsonLog -replace '`n',' '
+            $jsonLog = $jsonLog -replace '`t',' '
+            $jsonLog = $jsonLog -replace '\s+',' '
+            $jsonLog = ((($jsonLog.replace("\n"," ")).replace("\t"," ")).replace("\r"," "))
+            # log is stored locally in "queued.log" - the contents of this file are forwarded to the syslog server as soon as a connection to the server can be confirmed by ping probes 
+            $jsonLog >> queued.log
+        }
+    }
+}
+
+
+$global:time = (Get-Date) - (New-TimeSpan -Minutes 1)
+getStatus
+Remove-Variable -Name * -ErrorAction SilentlyContinue
+[gc]::collect()
